6b5ba9df96084db73ad9867739f7e7ab19842dc8
noelfoka <nomgnenoel@gmail.com>
2023-12-05 11:01:06 +0100

    create modal

--- src/components/AddNewTasks.js
+++ src/components/AddNewTasks.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal';
 
-const AddNewTasks = () => {
+function AddNewTasks () {
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div>AddNewTasks</div>
+    <div className='AddNewTodo'>
+      <div className='btn'>
+        <button onClick={() => setShowModal(true)}>
+          + New Task
+        </button>
+      </div>
+
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>
+          Hello world!
+        </div>
+
+        <button onClick={() => setShowModal(false)}>Hide</button>
+      </Modal>
+    </div>
   )
 }
 
