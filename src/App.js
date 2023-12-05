13bdacfaf1da5e1b2c7a57d3565ae455f94db60b
noelfoka <nomgnenoel@gmail.com>
2023-12-05 03:06:02 +0100

    create Header and Main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='app'>
+      <Header>
+
+      </Header>
+
+      <Main>
+
+      </Main>
     </div>
   );
 }
